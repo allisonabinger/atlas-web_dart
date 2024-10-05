46b8c8baf4997992bba80937be86992591ed0964
Allison <allisonabinger@gmail.com>
2024-10-05 16:56:52 -0500

    A - 10/05 04:56 PM - concatstrings works as intended, removed unnecessary parenthesis

---
+++ 0x01-dart-function/1-concat_strings.dart
@@ -0,0 +1,4 @@
+// prints two strings as input
+String concatStr(String str1, String str2) {
+  return '$str1 $str2';
+}
---
+++ 0x01-dart-function/1-main.dart
@@ -0,0 +1,6 @@
+// test for 1-concat_strings.dart
+import '1-concat_strings.dart';
+
+void main() {
+  print(concatStr("Holberton", "School"));
+}
