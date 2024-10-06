ad4596381f39772b347903f45658eacfa968d043
Allison <allisonabinger@gmail.com>
2024-10-06 15:53:46 -0500

    A - 10/06 03:53 PM - added password class to 6-password, imported into 6-inheritance

--- 0x02-dart-classes/6-inheritance.dart
+++ 0x02-dart-classes/6-inheritance.dart
@@ -1,29 +1,4 @@
-// contains the password and the User class
-class Password {
-  String _password = '';
-
-  Password({required String password}) : _password = password;
-
-  bool isValid() {
-    final validLength = _password.length >= 8 && _password.length <= 16;
-    final validUpperCase = _password.contains(RegExp(r'[A-Z]'));
-    final validLowerCase = _password.contains(RegExp(r'[a-z]'));
-    final validNumber = _password.contains(RegExp(r'[0-9]'));
-
-    return validLength && validUpperCase && validLowerCase && validNumber;
-  }
-
-  String get password {
-    return _password;
-  }
-
-  void set password(String newPass) {
-    this._password = newPass;
-  }
-
-  @override
-  String toString() => 'Your Password is: $_password';
-}
+import '6-password.dart';
 
 class User extends Password {
   String name ;
