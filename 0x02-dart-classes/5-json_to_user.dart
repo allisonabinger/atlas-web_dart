409133d635021d4dedb9148eb88735a04d448d30
Allison <allisonabinger@gmail.com>
2024-10-06 15:12:19 -0500

    A - 10/06 03:11 PM - Added fromJson to user class, included id

---
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -0,0 +1,34 @@
+// contains the User class
+
+class User {
+  String name ;
+  int age;
+  double height;
+  int id;
+
+  User({required this.id, required this.name, required this.age, required this.height});
+
+  // keys are strings, values are dynamic (any type)
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'age': age,
+      'height': height,
+    };
+  }
+
+  static User fromJson(Map<dynamic, dynamic> userJson) {
+    return User(
+      id: userJson['id'],
+      name: userJson['name'],
+      age: userJson['age'],
+      height: userJson['height']
+    );
+  }
+
+    @override
+    String toString() {
+      return 'User(id: $id, name: $name, age: $age, height: $height)';
+    }
+}
